372ef9d132adad64bf5a7c330d92669573c41de3
dxu5 <42157345+dxu5@users.noreply.github.com>
2020-07-16 21:03:18 -0700

    Created Login Page, Error Page, and Page Layout

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Gihub Search Project
---
+++ src/pages/Login.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
+import bg from "./img/bg.jpg";
+import moon from "./img/moon.png";
+import mountain from "./img/mountain.png";
+import road from "./img/road.png";
+class Login extends React.Component {
+  componentDidMount() {
+    let bg = document.getElementById("bg");
+    let moon = document.getElementById("moon");
+    let mountain = document.getElementById("mountain");
+    let road = document.getElementById("road");
+    let text = document.getElementById("text");
+
+    window.addEventListener("scroll", function () {
+      var value = window.scrollY;
+      bg.style.top = value * 0.5 + "px";
+      moon.style.left = -value * 0.5 + "px";
+      mountain.style.top = -value * 0.15 + "px";
+      road.style.top = value * 0.15 + "px";
+      text.style.top = value * 1 + "px";
+    });
+  }
+  render() {
+    return (
+      <Wrapper>
+        <section>
+          <img src={bg} alt="starry background" id="bg"></img>
+          <img src={moon} alt="moon" id="moon"></img>
+          <img src={mountain} alt="mountain" id="mountain"></img>
+          <img src={road} alt="road" id="road"></img>
+          <h2 id="text">Github Search</h2>
+        </section>
+        <div className="container">
+          <button className="btn">Login</button>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+const Wrapper = styled.section`
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
+  background: #0a2a43;
+  min-height: 1500px;
+  section {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  section:before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 100px;
+    background: linear-gradient(to top, #0a2a43, transparent) !important;
+    z-index: 100000;
+  }
+  section:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #0a2a43;
+    z-index: 100000;
+    mix-blend-mode: color;
+  }
+  section img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    pointer-events: none;
+  }
+  #text {
+    position: relative;
+    color: #fff;
+    font-size: 10em;
+    z-index: 1;
+  }
+  #road {
+    z-index: 2;
+  }
+  .container {
+    text-align: center;
+  }
+`;
+export default Login;
